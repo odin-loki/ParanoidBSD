35224a80f9b2d2bf0b9d8dd7d6c981266291c145
Odin Loch <odin.loch@outlook.com.au>
2026-07-30 19:30:04 +1000

    pbsd: 236 files ported mechanically, proven by IR equivalence

---
+++ pbsd/lib/libc/gen/timespec_getres_m/oracle.c
@@ -0,0 +1,24 @@
+/*-
+ * Copyright (c) 2023 Dag-Erling Smørgrav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <time.h>
+
+int
+ref_timespec_getres(struct timespec *ts, int base)
+{
+
+	switch (base) {
+	case TIME_UTC:
+		if (clock_getres(CLOCK_REALTIME, ts) == 0)
+			return (base);
+		break;
+	case TIME_MONOTONIC:
+		if (clock_getres(CLOCK_MONOTONIC, ts) == 0)
+			return (base);
+		break;
+	}
+	return (0);
+}
